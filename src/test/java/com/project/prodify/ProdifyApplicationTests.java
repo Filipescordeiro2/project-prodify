0a98c34a5870d2ebf873f6d85560ecfcdf4e1ed7
Filipescordeiro2 <filipescordeiro@hotmail.com>
2025-01-19 22:36:45 -0300

    Commit Inical

---
+++ src/test/java/com/project/prodify/ProdifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.prodify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
